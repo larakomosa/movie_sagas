17826e06b50cad28050efb6cfe16d1311ff3a09f
Lara Komosa <larakomosa@gmail.com>
2020-11-14 16:07:21 -0600

    some basic styling before trying material ui

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css'; //importing bootstrap for formatting
 import './App.css';
 
 import Home from '../../pages/Home/Home';
 import Details from '../../pages/Details/Details';
 import Add from '../../pages/Add-Movie/Add-Movie';
+import { makeStyles } from '@material-ui/core/styles';
 
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
     return (
-      <div className="App">
-        <h1>Movies!</h1>
-        <Router>
+      <Router>
+        <div className="App">
+          <div className="header">
+            <h2>Blockbusters</h2>
+            <nav>
+              <h4>
+                <Link to="/">HOME</Link>
+              </h4>
+              <h4>
+                <Link to="/add"> ADD MOVIES</Link>
+              </h4>
+            </nav>
+          </div>
           <Route exact path="/" component={Home} />
           <Route exact path="/details" component={Details} />
           <Route exact path="/add" component={Add} />
-        </Router>
-      </div>
+        </div>
+      </Router>
     );
   }
 }
